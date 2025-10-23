e4886c6ec3675073420d7b8103d2c9dc7a38483c
shahin <schahinheidari@gmail.cm>
2025-10-23 21:16:27 +0200

    Added postman collection

--- src/main/java/com/example/todo/model/entity/User.java
+++ src/main/java/com/example/todo/model/entity/User.java
@@ -9,7 +9,7 @@ import java.util.*;
 @Data
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true)
